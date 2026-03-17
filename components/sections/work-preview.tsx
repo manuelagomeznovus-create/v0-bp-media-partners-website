93f19fdfdcbff9e88c39034a7342b2673e875cb2
manuelagomeznovus-create <manuelagomez.novus@gmail.com>
2026-03-17 19:41:11 -0400

    Update work-preview.tsx

--- components/sections/work-preview.tsx
+++ components/sections/work-preview.tsx
@@ -8,7 +8,7 @@ import { ArrowRight } from "lucide-react"
 
 const projects = [
   {
-    title: "Apple Product Launch",
+    title: "Samsung S26 Product Launch",
     category: "Technology Event",
     image: "https://images.unsplash.com/photo-1540575467063-178a50c2df87?q=80&w=2070&auto=format&fit=crop",
   },
