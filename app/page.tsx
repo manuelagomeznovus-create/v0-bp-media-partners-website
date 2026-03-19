c438c29aa91763eababd5efbcc1c3f44e9d189a2
manuelagomeznovus-create <manuelagomez.novus@gmail.com>
2026-03-19 13:33:49 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,35 +1,3 @@
-import { Navigation } from "@/components/navigation"
-import { Footer } from "@/components/footer"
-import { HeroSection } from "@/components/sections/hero"
-import { StatsSection } from "@/components/sections/stats"
-import { AboutPreviewSection } from "@/components/sections/about-preview"
-import { ServicesPreviewSection } from "@/components/sections/services-preview"
-import { WorkPreviewSection } from "@/components/sections/work-preview"
-import { GalleryPreviewSection } from "@/components/sections/gallery-preview"
-import { TeamSection } from "@/components/sections/team"
-import { ApproachSection } from "@/components/sections/approach"
-import { GlobalPresenceSection } from "@/components/sections/global-presence"
-import { ClientsSection } from "@/components/sections/clients"
-import { CTASection } from "@/components/sections/cta"
-
-export default function HomePage() {
-  return (
-    <>
-      <Navigation />
-      <main>
-        <HeroSection />
-        <StatsSection />
-        <AboutPreviewSection />
-        <ServicesPreviewSection />
-        <WorkPreviewSection />
-        <GalleryPreviewSection />
-        <TeamSection />
-        <ApproachSection />
-        <GlobalPresenceSection />
-        <ClientsSection />
-        <CTASection />
-      </main>
-      <Footer />
-    </>
-  )
+export default function Home() {
+  return <h1>OK</h1>
 }
