94bf208419bbb721e0cd6b0f8dc3a5912cda31e1
manuelagomeznovus-create <manuelagomez.novus@gmail.com>
2026-03-23 12:26:47 -0400

    Update Claudia Alvarado's image path

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -35,7 +35,8 @@ const team = [
     name: "Claudia Alvarado",
     role: "Digital Marketing Manager",
     bio: "Leads digital strategy, including online campaigns, social media, and brand presence. Focused on delivering impactful, engagement-driven marketing solutions.",
-    image: "/images/claudia_socialmedia.jpg"
+    image: "/images/claudianewbp.jpg"
+
   },
   {
     name: "Manuela Gomez",
