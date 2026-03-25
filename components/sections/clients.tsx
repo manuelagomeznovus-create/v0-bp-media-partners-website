4937a4e292a59c3b85c08c6ac8c24b180a5f6fbd
manuelagomeznovus-create <manuelagomez.novus@gmail.com>
2026-03-25 15:06:00 -0400

    Update clients.tsx

--- components/sections/clients.tsx
+++ components/sections/clients.tsx
@@ -41,7 +41,7 @@ export function ClientsSection() {
      <img
   src={client.logo}
   alt={client.name}
-  className="h-25 w-auto object-contain mx-auto"
+  className="h-30 w-auto object-contain mx-auto"
  />
     </div>
   ))}
